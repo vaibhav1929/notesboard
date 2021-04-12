8d25f70e3abd01e1f307906bbc262a94754e44f5
Deep_Menpara <54411124+Deep-Menpara@users.noreply.github.com>
2021-04-12 19:27:45 +0530

    dbmanager methods add

--- src/main/java/com/ajava/notesboard/controllers/DBManager.java
+++ src/main/java/com/ajava/notesboard/controllers/DBManager.java
@@ -1,16 +1,18 @@
 package com.ajava.notesboard.controllers;
 
 
+import com.ajava.notesboard.models.Notes;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.ajava.notesboard.models.User;
+import java.util.ArrayList;
 /*
  * @author "Vaibhav Sorathiya (19BCE529)"
  */
 public class DBManager {
     Connection con;
-    PreparedStatement insertAccount, updateAccount, loginAccount, emailAccount;
+    PreparedStatement insertAccount, updateAccount, loginAccount, emailAccount, notesInsert, notesUpdate, notesDelete,getNotesByUidStatment,getNotesByUidAndNidStatment;
     
     public DBManager(){
         if(con == null){
@@ -24,17 +26,133 @@ public class DBManager {
                 }
                 insertAccount = con.prepareStatement("INSERT INTO Users(name,email,password,pin) VALUES(?,?,?,?)");
                 
-                updateAccount = con.prepareStatement("UPDATE Users SET name = ?, password = ? WHERE uid = ?");
+                updateAccount = con.prepareStatement("UPDATE Users SET name = ?, email = ?, password = ?, pin = ?, isVerified = ?, vaultPassword = ? WHERE uid = ?");
                 
                 loginAccount = con.prepareStatement("SELECT * FROM Users WHERE email = ? AND password = ?");
                 
                 emailAccount = con.prepareStatement("SELECT * FROM Users WHERE email = ?");
+                
+                notesInsert = con.prepareStatement("INSERT INTO Notes(groupid,uid,title,type,content,colorcode) VALUES(?,?,?,?,?,?)");
+                
+                notesUpdate = con.prepareStatement("UPDATE Notes SET groupid = ?, uid = ?, title = ?, type = ?, content = ?, colorcode = ?, isdeleted = ?, whendeleted = ? WHERE nid = ?");
+                
+                notesDelete = con.prepareStatement("DELETE FROM Notes WHERE nid = ?");
+                
+                getNotesByUidStatment = con.prepareStatement("SELECT * FROM Notes WHERE uid = ?");
+                
+                getNotesByUidAndNidStatment = con.prepareStatement("SELECT * FROM Notes WHERE uid = ? AND nid = ?");
+                
                
             }catch(Exception e){
                 System.err.println(e);
             }
             
         }
+    }
+    
+    public ArrayList<Notes> getNotesByUid(int uid){
+        ArrayList<Notes> ret=new ArrayList<Notes>();
+        try {
+            getNotesByUidStatment.setInt(1, uid);
+            ResultSet set = emailAccount.executeQuery();
+            while(set.next()){
+                Notes temp=new Notes(set.getInt("nid"),set.getInt("groupid"),set.getInt("uid"),set.getString("title"),set.getString("type"),set.getString("content"),set.getString("colorcode"),set.getBoolean("isdeleted"),set.getTimestamp("whendeleted").toString());
+                ret.add(temp);
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+    }
+    public Notes getNotesByUidAndNid(int uid,int nid){
+        Notes temp=new Notes();
+        try {
+            getNotesByUidAndNidStatment.setInt(1, uid);
+            getNotesByUidAndNidStatment.setInt(2, nid);
+            ResultSet set = emailAccount.executeQuery();
+            while(set.next()){
+                 temp=new Notes(set.getInt("nid"),set.getInt("groupid"),set.getInt("uid"),set.getString("title"),set.getString("type"),set.getString("content"),set.getString("colorcode"),set.getBoolean("isdeleted"),set.getTimestamp("whendeleted").toString());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return temp;
+    }
+    
+    
+    
+    public boolean deleteNote(int nid){
+        try {
+            notesUpdate.setInt(1, nid);
+            int rows = notesUpdate.executeUpdate();
+            return rows > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       return false;
+    }
+    
+        public boolean updateNote(Notes note){
+        try {
+            notesUpdate.setInt(1, note.getGroupid());
+            notesUpdate.setInt(2, note.getUid());
+            notesUpdate.setString(3, note.getTitle());
+            notesUpdate.setString(4, note.getType());
+            notesUpdate.setString(5, note.getContent());
+            notesUpdate.setString(6, note.getColorcode());
+            notesUpdate.setBoolean(7, note.isIsdeleted());
+            Long miliseconds = Long.valueOf(note.getWhendeleted());
+            Timestamp ti = new Timestamp(miliseconds);
+            notesUpdate.setTimestamp(8, ti);
+            notesUpdate.setInt(9, note.getNid());
+            int rows = notesUpdate.executeUpdate();
+            return rows > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       return false;
+    }
+    
+    
+    
+    
+        public boolean addNewNote(Notes note){
+        try {
+            notesInsert.setInt(1, note.getGroupid());
+            notesInsert.setInt(2, note.getUid());
+            notesInsert.setString(3, note.getTitle());
+            notesInsert.setString(4, note.getType());
+            notesInsert.setString(5, note.getContent());
+            notesInsert.setString(6, note.getColorcode());
+            int rows = notesInsert.executeUpdate();
+            return rows > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       return false;
+    }
+    
+
+            public boolean updateUser(User user){
+        try {
+            updateAccount.setString(1, user.getName());
+            updateAccount.setString(2, user.getEmail());
+            updateAccount.setString(3, user.getPassword());
+            updateAccount.setInt(4, user.getPin());
+            updateAccount.setBoolean(5, user.isIsVerified());
+            updateAccount.setString(6, user.getVaultPassword());
+            updateAccount.setInt(7, user.getUid());
+            int rows = updateAccount.executeUpdate();
+            return rows > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       return false;
     }
     
     public boolean addNewUser(User user){
@@ -51,6 +169,10 @@ public class DBManager {
         
        return false;
     }
+    
+
+    
+    
     public User getUserByEmail(String email){
         User user = new User();
         try {
